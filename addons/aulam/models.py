46385673ce2adafd8beae54859110e63cdcb49b0
Felipe Martínez <felipe@galicia.ugt.org>
2015-12-07 17:48:35 +0100

    20151207:forms one2many

--- addons/aulam/models.py
+++ addons/aulam/models.py
@@ -16,26 +16,26 @@ class aulamAlumno(models.Model):
     id_tarifa = fields.Many2one('aulam.tarifa','Tarifa')
     id_curso = fields.Many2one('aulam.curso','Curso')
     id_calificacion = fields.One2many('aulam.calificacion','id_alumno','Calificacion')
-    id_horario = fields.One2many('aulam.horario','id_alumno','Horio')
-    fAlta = fields.Date()
-    fBaja = fields.Date()
-    hExtra = fields.Float()
-    anno = fields.Char()
-    asignaturas = fields.Char()
+    id_horario = fields.One2many('aulam.horario','id_alumno','Horario')
+    fAlta = fields.Date(string="Fecha de Alta")
+    fBaja = fields.Date(string="Fecha de Baja")
+    hExtra = fields.Float(string="Horas Extra")
+    anno = fields.Char("Año académico")
+    asignaturas = fields.Char(string="Asignaturas")
     _rec_name = 'id_partner'
 
 
 class aulamColegio(models.Model):
     _name = 'aulam.colegio'
-    nomColegio = fields.Char()
-    email = fields.Char()
-    tlf = fields.Char()
+    nomColegio = fields.Char(string="Nombre del colegio")
+    email = fields.Char(string="Correo Electrónico")
+    tlf = fields.Char(string="Teléfono")
     _rec_name = 'nomColegio'
 
 class aulamCurso(models.Model):
     _name = 'aulam.curso'
-    orden = fields.Char()
-    ciclo = fields.Char()
+    orden = fields.Char(string="Orden de curso")
+    ciclo = fields.Char(string="Nombre del ciclo")
 
     def name_get(self, cr, uid, ids, context={}):
         if not len(ids):
@@ -47,25 +47,25 @@ class aulamCurso(models.Model):
 
 class aulamTarifa(models.Model):
     _name = 'aulam.tarifa'
-    nomTarifa = fields.Char()
-    importe = fields.Float()
+    nomTarifa = fields.Char(string="Tarifa")
+    importe = fields.Float(digits=(6, 2), string="Importe")
     _rec_name = 'nomTarifa'
     
 class aulamCalificacion(models.Model):
     _name = 'aulam.calificacion'
     id_alumno = fields.Many2one('aulam.alumno','Alumno')
-    asignatura = fields.Char()
-    fecha = fields.Date()
-    calificacion = fields.Float()
-    anno = fields.Char()
+    asignatura = fields.Char(string="Asigantura")
+    fecha = fields.Date(string="Fecha de la calificación")
+    calificacion = fields.Float(string="Calificación")
+    anno = fields.Char(string="Curso Académico")
     
 class aulamHorario(models.Model):
     _name = 'aulam.horario'
     id_alumno = fields.Many2one('aulam.alumno','Alumno')
-    anno = fields.Char()
-    wDay = fields.Char()
-    checkin = fields.Char()
-    checkout = fields.Char()
+    anno = fields.Char(string="Curso Académico")
+    wDay = fields.Char(string="Día de la semana")
+    checkin = fields.Char(string="Entrada")
+    checkout = fields.Char(string="Salida")
 
 
     #partner_id = fields.many2
